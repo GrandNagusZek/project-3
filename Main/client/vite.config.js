6d38d92e619ff85ad561db850022ff50218ae9d2
GrandNagusZek <iliyas.mallard@yahoo.com>
2024-06-02 15:46:47 -0400

    installed vite pwa plugin and started work on code to configure Vite to use the pwa plugin

--- Main/client/vite.config.js
+++ Main/client/vite.config.js
@@ -1,9 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA({
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'AthleticMia',
+      short_name: 'App',
+      description: 'Exercise equipment for your needs',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'icon-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+  })],
   server: {
     port: 3000,
     open: true,
