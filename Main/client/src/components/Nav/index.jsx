572abce5f31e4a27b2dabf0af1a9297d589bb4fc
deivid how <vvdoxi@gmail.com>
2024-06-03 18:36:31 -0400

    did some final touches

--- Main/client/src/components/Nav/index.jsx
+++ Main/client/src/components/Nav/index.jsx
@@ -36,7 +36,7 @@ function Nav() {
         <h1>
           <Link to="/">
             <span role="img" aria-label="shopping bag">
-              🛍️
+              
             </span>
             AthleticMia
           </Link>
